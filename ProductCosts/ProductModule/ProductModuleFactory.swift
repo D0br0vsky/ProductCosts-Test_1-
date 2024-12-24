126e1a1458a97da173d2bb54333d8e609c84041f
Avrely <miron.dobrovsky.work@gmail.com>
2024-12-24 15:46:43 +0300

    fixed bugs

--- ProductCosts/ProductModule/ProductModuleFactory.swift
+++ ProductCosts/ProductModule/ProductModuleFactory.swift
@@ -1,26 +1,40 @@
-
 import UIKit
 
 final class ProductModuleFactory {
-    
+
+    private static func makeDataLoader() -> DataLoader {
+        return DataLoader()
+    }
+
+    private static func makeDataService(dataLoader: DataLoader) -> DataService {
+        return DataService(loadData: dataLoader)
+    }
+
+    private static func makeRatesDataStorage(service: DataService) -> RatesDataStorage {
+        return RatesDataStorage(service: service)
+    }
+
     static func makeWithInitializedRates(completion: @escaping (UIViewController) -> Void) {
-        let loadData = DataLoader()
-        let service = DataService(loadData: loadData)
-        let ratesDataStorage = RatesDataStorage(service: service)
+        let dataLoader = makeDataLoader()
+        let service = makeDataService(dataLoader: dataLoader)
+        let ratesDataStorage = makeRatesDataStorage(service: service)
+        
         ratesDataStorage.ratesLoad {
             let vc = ProductModuleFactory.make(ratesDataStorage: ratesDataStorage)
             completion(vc)
         }
     }
-    
+
     static func make(ratesDataStorage: RatesDataStorageProtocol) -> UIViewController {
         let factory = TransactionModuleFactory(ratesDataStorage: ratesDataStorage)
-        let dataLoader = DataLoader()
+        let dataLoader = makeDataLoader()
+        let service = makeDataService(dataLoader: dataLoader)
+        let dataStorage = makeRatesDataStorage(service: service)
+        
         let router = ProductModuleRouter(factory: factory)
-        let service = DataService(loadData: dataLoader)
-        let dataStorage = RatesDataStorage(service: service)
         let presenter = ProductModulePresenter(service: service, router: router, dataStorage: dataStorage)
         let vc = ProductModuleController(presenter: presenter)
+        
         router.setRoot(vc)
         presenter.view = vc
         return vc
